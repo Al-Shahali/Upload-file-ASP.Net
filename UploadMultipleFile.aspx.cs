6652265c70702fb975bdc3a27c5ca32107f4878e
Al-Shahali <61845394+Al-Shahali@users.noreply.github.com>
2021-12-13 17:22:15 +0200

    Add files via upload

---
+++ UploadMultipleFile.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppFirst
+{
+    public partial class UploadMultipleFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Btn_upload_click(object sender, EventArgs e)
+        {
+            if (fileupload.PostedFile != null && fileupload.PostedFile.ContentLength > 0)
+            {
+                var count = 0;
+                foreach (HttpPostedFile uploadedfile in fileupload.PostedFiles)
+                {
+                    string fn = System.IO.Path.GetFileName(uploadedfile.FileName);
+                    System.Diagnostics.Debug.Write("File Name : " + fn);
+                    string SaveLocation = Server.MapPath("upload") + "\\" + fn;
+                    try
+                    {
+                        fileupload.SaveAs(SaveLocation);
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        FileUploadStatus.Text = "error : " + ex.Message;
+                    }
+                }
+                if (count > 0)
+                {
+                    FileUploadStatus.Text = count + " files has been uploaded.";
+                }
+            }
+            else
+            {
+                FileUploadStatus.Text = "Please select a file to upload.";
+            }
+        }
+    }
+}
